d8cdd6883820d4bf3257a92c0647db6c8ecc5974
Nathan Addison <itmademe49@gmail.com>
2025-02-08 11:12:44 +1300

    update source index file with paths

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/bsky-txt2img">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
