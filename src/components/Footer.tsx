5fabffe91b1d757cebf9d9f44182afd5e634a656
Nathan Addison <itmademe49@gmail.com>
2025-02-08 11:46:32 +1300

    add link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,23 @@
-
-import { Github, Twitter } from "lucide-react";
+import { Code } from "lucide-react";
 import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <footer className="w-full border-t bg-white">
       <div className="max-w-7xl mx-auto px-4 h-14 flex items-center justify-center space-x-8">
         <p className="text-sm text-muted-foreground">No data is stored or collected from this application</p>
-
         <div className="flex items-center space-x-4">
           <p className="text-sm text-muted-foreground">&copy; {new Date().getFullYear()}</p>
           <div className="flex space-x-1">
-            <Button variant="ghost" size="sm">
-              <Github className="h-4 w-4" />
-            </Button>
-            <Button variant="ghost" size="sm">
-              <Twitter className="h-4 w-4" />
+            <Button
+              variant="ghost"
+              size="sm"
+              asChild
+            >
+              <a href="https://github.com/r3cla/bsky-txt2img" target="_blank" rel="noopener noreferrer">
+                <Code className="h-4 w-4" />
+              </a>
             </Button>
           </div>
         </div>
